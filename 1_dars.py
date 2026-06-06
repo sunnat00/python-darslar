cd7eb91cae56ab4c2f521d70638eb72380eb486f
sunnatisroilov0-spec <sunnatisroilov0@gmail.com>
2026-06-06 23:15:19 +0200

    birinchi dars

---
+++ 1_dars.py
@@ -0,0 +1,20 @@
+print("Hello world!")
+
+
+print("Hello world!")
+
+
+print("salom duno_3")
+
+# print("hellonworld!")
+
+# print(123 + 456)
+# print(12 - 123)
+# print(12 / 12)
+# print(3*8)
+# print(12 ** 4)
+# print(12 // 5)
+# print(12 % 5)
+# print(12 // 12)
+# print(12 % 12)
+print("Natija: ",12 * 12 + 5 - 3 / 3)
