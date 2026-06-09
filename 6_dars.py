2c6be6058ab36856db493ce4ebdcda71c0f1a870
sunnatisroilov0-spec <sunnatisroilov0@gmail.com>
2026-06-09 15:32:20 +0200

    6 dars

---
+++ 6_dars.py
@@ -0,0 +1,87 @@
+#  shart operation ( if, else)
+#IF => Agar
+#else => aks holda   ----- agarga negatif
+
+
+
+
+#a = int(input("son_1: "))
+#b = int(input("son_2: "))
+
+
+
+#if a > b :
+ #   print("A soni katta ")
+#
+#if a ==b :
+#    print("Teng A va B")
+#else:
+ #   print(" B soni katta")
+
+
+
+
+
+#bugun elif - yoki 
+# 
+# 
+# if => Agar
+# elif=> Yoki
+# else => Aks holda
+#     
+
+
+
+
+
+#a = int(input("son_1: "))
+#b = int(input("son_2: "))
+#if a > b :
+#   print("A katta")
+#elif a == b :
+#   print("Teng")
+#else:
+ #   print("B katta") 
+ # 
+ # 
+ # 
+ # 
+
+
+#number =str(input("Telefon raqam: "))  
+#if len(number) == 8:
+#    print("Siz royxatdan o'tdingiz")
+#elif len(number) > 8:
+#   print("Siz qaytadan urinishingiz kerak")
+
+#else:
+#    print("Nimadir kam")
+
+
+
+
+
+
+
+matn = str(input("Matn: "))
+if matn == "salom":
+    print("salom qaleysan")
+elif matn == "ali" :
+    print("Ali qalaysan")
+elif matn == "zur" :
+    print("Men ham zur")  
+elif matn == "Python" :
+    print("Python 3.12") 
+else: 
+    print("Bunaqa malumot yoq")       
+     
+
+
+
+
+
+
+
+
+
+
