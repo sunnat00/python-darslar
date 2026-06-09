d73eb2fe3dcdd345e590cfcffd6f5c8391fad6d2
sunnatisroilov0-spec <sunnatisroilov0@gmail.com>
2026-06-09 13:44:00 +0200

    3 dars tugadi

--- 3_darss.py
+++ 3_darss.py
@@ -1,16 +1,62 @@
-matn = "dars"
+#matn = "dars"
 # print(matn)
-print(type(matn))
-print(matn)
+#print(type(matn))
+#print(matn)
 
 
 
-ikkinchi_matn = "string haqida"
+#ikkinchi_matn = "string haqida"
 # str - string - matn
 # str - data type - ma'alumot turi
 # matn = "dars"
-print(matn)
-print(type(matn))
+#print(matn)
+#print(type(matn))
+
+
+
+
+# float  kassir sonlar 1.2
+#a = 0.1
+#print(type(a))
+
+
+# bool
+#a = 50
+#b = 9
+#c = a > b
+#print(type(c), c)
+
+
+
+
+
+# int va float qoshilsa har doim float boladi
+#a = 78 
+#b = 0.5
+#c = a + b
+#print(type(c))
+
+
+
+
+
+# initni hech qachon stringa qoshib bolmaydi
+#a = 45
+#b = "salom"
+#c = a + b
+#print(c) 
+
+
+
+
+
+
+
+# int - Number
+
+#a = -45
+#print(a)
+#print(type(a))
 
 
 
