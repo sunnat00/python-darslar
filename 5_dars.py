d1b7d0550b626ddb2acb6fe94272ba90efdbab2d
sunnatisroilov0-spec <sunnatisroilov0@gmail.com>
2026-06-09 14:35:33 +0200

    5 dars

---
+++ 5_dars.py
@@ -0,0 +1,51 @@
+# bugun stringlar haqida gaplashamiz
+# upperni vazifasi hamma harflarni katta qilib beradi
+
+
+matn = str(input("Matn:"))
+
+#a = matn.upper()
+#print(a)
+
+
+
+
+# Capitalize() bu faqat bosh harfni katta qilish uchun ishlatiladi
+#a = matn.capitalize()
+#print(a)
+
+
+#  Lower()  bu faqat katta harflarda yozilgan harflarni katta qilib beradi
+# upperni qarama qrshisi va hamma harflarni kichkina qilib beradi
+#  
+#a = matn.lower()
+#print(a)
+
+
+
+#matn = str(input("Matn:"))
+
+#a = matn.isupper()
+#print(a)
+
+
+
+#a = matn.islower()
+#print(a)
+
+
+
+# count() bu sozlarni ichida belgilagan harflarni sanab berar ekan
+
+#a = matn.count("a")
+#print(a)
+
+
+
+
+
+
+# index()   bu sozlarni ichidagi bitta harfni belgilab olsak shu harfni nechinchi harfligini aytib berar ekan
+
+a = matn.index("b")
+print(a)
