ccca8005ff2bfc8f4053395de2cfa933eefc34bb
sunnatisroilov0-spec <sunnatisroilov0@gmail.com>
2026-06-09 14:06:26 +0200

    4 dars

---
+++ 4_dars.py
@@ -0,0 +1,31 @@
+# bugun input haqida gaplashamiz 
+
+# input()
+#a = str(input("Matn:"))
+#print(a)
+
+
+#b = int(input("Son:"))
+#print(b)
+
+#c = float(input("Son"))
+#print(c)
+
+
+
+name = str(input("Name:"))
+job = str(input("Job:"))
+now_year = int(input("This year:"))
+year = int(input(" Born year"))
+c = now_year -  year
+
+
+
+print(" mijozni ismi: ", name)
+print(" mijozni job: ", job)
+print(" mijozni born year: ", year)
+print(" mijozni age: ",c )
+
+
+
+
