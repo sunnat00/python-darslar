f4f0f0f02d63ec736ffd4b988636d8380db82474
sunnatisroilov0-spec <sunnatisroilov0@gmail.com>
2026-06-12 23:08:20 +0200

    LOOP FOR

--- 7_dars.py
+++ 7_dars.py
@@ -1,2 +1,18 @@
-for i in range(1, 11):
+#for i in range(1, 11):
+ #   print(i)
+
+#for => kalit suz
+#i => o'zgaruvchi 
+# in
+# range i + 0
+#for i in range(11):
+#    print(i) 
+
+for i in range(5, 11):
     print(i)
+
+
+
+
+
+
