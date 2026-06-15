3dc2cb79fbb6281b23d3178aff025157d7722bdd
sunnatisroilov0-spec <sunnatisroilov0@gmail.com>
2026-06-16 00:17:03 +0200

    8 dars yarmi

--- 8_dars.py
+++ 8_dars.py
@@ -1,4 +1,19 @@
-a = "salom"
-b = "ali"
+#a = "Dunyo"
+#b = "ali"
 
-print(F"Salom {a} nima gap")
+#print(F"Salom {a} nima gap {b}")
+#print(f"salom {b} nima gap")
+
+
+a = int(input("son_1: "))
+b = int(input("son_2: "))
+
+#print(f"{a + b} Yigindi\n{a - b} Ayirma")
+
+
+if a > b:
+    print(f"{a} Soni katta ekan")
+elif a == b:
+    print(f"{a} va {b} sonlari teng") 
+else:
+    print(f"{b} soni katta ekan")       
